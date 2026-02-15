95cf18c806d20fc15af0967e2073ffc161fe2f9b
theAman30 <a.srivastav1005@gmail.com>
2026-02-15 15:43:22 +0530

    a

--- js/main.js
+++ js/main.js
@@ -1,170 +1,178 @@
 const navbar = document.getElementById("navbar");
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 60) {
-      navbar.classList.add(
-        "backdrop-blur-lg",
-        "bg-white/80",
-        "dark:bg-[#0f172a]/80",
-        "shadow-sm"
-      );
-    } else {
-      navbar.classList.remove(
-        "backdrop-blur-lg",
-        "bg-white/80",
-        "dark:bg-[#0f172a]/80",
-        "shadow-sm"
-      );
-    }
-  });
-
-
-  const menuBtn = document.getElementById("menu-btn");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
-
-//   particle.js 
-        particlesJS("particles-js", {
-
-            particles: {
-                number: {
-                    value: 150,
-                    density: {
-                        enable: true,
-                        value_area: 900
-                    }
-                },
-
-                color: {
-                    value: "#ff7613"
-                },
-
-                shape: {
-                    type: "circle"
-                },
-
-                opacity: {
-                    value: 0.8,
-                    random: true
-                },
-
-                size: {
-                    value: 3,
-                    random: true
-                },
-
-                line_linked: {
-                    enable: true,
-                    distance: 160,
-                    color: "#ff7613",
-                    opacity: 0.4,
-                    width: 1
-                },
-
-                move: {
-                    enable: true,
-                    speed: 3.5,
-                    direction: "none",
-                    random: false,
-                    straight: false,
-                    out_mode: "out"
-                }
-            },
-
-            interactivity: {
-                detect_on: "canvas",
-
-                events: {
-                    onhover: {
-                        enable: true,
-                        mode: "grab"
-                    },
-                    onclick: {
-                        enable: true,
-                        mode: "push"
-                    },
-                    resize: true
-                },
-
-                modes: {
-                    grab: {
-                        distance: 200,
-                        line_linked: {
-                            opacity: 1
-                        }
-                    },
-
-                    push: {
-                        particles_nb: 5
-                    }
-                }
-            },
-
-            retina_detect: true
-        });
-
-
-var typed = new Typed(".typing-text", {
-    strings: [
-        "Frontend Developer",
-        "Freelancer",
-        "Graphic Designer",
-        "Video Editor",
-        "Frontend & Backend Systems"
-    ],
-    typeSpeed: 60,
-    backSpeed: 35,
-    backDelay: 1200,
-    loop: true,
-    smartBackspace: true,
-    showCursor: true,
-    cursorChar: "|"
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    navbar.classList.add(
+      "backdrop-blur-lg",
+      "bg-white/80",
+      "dark:bg-[#0f172a]/80",
+      "shadow-sm",
+    );
+  } else {
+    navbar.classList.remove(
+      "backdrop-blur-lg",
+      "bg-white/80",
+      "dark:bg-[#0f172a]/80",
+      "shadow-sm",
+    );
+  }
 });
 
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 
-// social media icons hides when reaches footer 
+//   particle.js
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 900,
+      },
+    },
+
+    color: {
+      value: "#ff7613",
+    },
+
+    shape: {
+      type: "circle",
+    },
+
+    opacity: {
+      value: 0.8,
+      random: true,
+    },
+
+    size: {
+      value: 3,
+      random: true,
+    },
+
+    line_linked: {
+      enable: true,
+      distance: 160,
+      color: "#ff7613",
+      opacity: 0.4,
+      width: 1,
+    },
+
+    move: {
+      enable: true,
+      speed: 3.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+    },
+  },
+
+  interactivity: {
+    detect_on: "canvas",
+
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+
+    modes: {
+      grab: {
+        distance: 200,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+
+      push: {
+        particles_nb: 5,
+      },
+    },
+  },
+
+  retina_detect: true,
+});
+
+var typed = new Typed(".typing-text", {
+  strings: [
+    "Frontend Developer",
+    "Freelancer",
+    "Graphic Designer",
+    "Video Editor",
+    "Frontend & Backend Systems",
+  ],
+  typeSpeed: 60,
+  backSpeed: 35,
+  backDelay: 1200,
+  loop: true,
+  smartBackspace: true,
+  showCursor: true,
+  cursorChar: "|",
+});
+
+// social media icons hides when reaches footer
 const socialBar = document.querySelector(".social-bar");
 const footer = document.getElementById("footer");
 
 window.addEventListener("scroll", () => {
+  const footerTop = footer.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
 
-    const footerTop = footer.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    // Only apply on laptop / desktop
-    if (window.innerWidth > 1200) {
-
-        if (footerTop < windowHeight - 100) {
-            socialBar.style.opacity = "0";
-            socialBar.style.pointerEvents = "none";
-        } else {
-            socialBar.style.opacity = "1";
-            socialBar.style.pointerEvents = "auto";
-        }
-
+  // Only apply on laptop / desktop
+  if (window.innerWidth > 1200) {
+    if (footerTop < windowHeight - 100) {
+      socialBar.style.opacity = "0";
+      socialBar.style.pointerEvents = "none";
+    } else {
+      socialBar.style.opacity = "1";
+      socialBar.style.pointerEvents = "auto";
     }
-
+  }
 });
 
-        // Magnetic Hover Effect
-        const socialItems = document.querySelectorAll(".social-item");
+// Magnetic Hover Effect
+const socialItems = document.querySelectorAll(".social-item");
 
-        socialItems.forEach(item => {
+socialItems.forEach((item) => {
+  item.addEventListener("mousemove", function (e) {
+    const rect = item.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
 
-            item.addEventListener("mousemove", function (e) {
-                const rect = item.getBoundingClientRect();
-                const x = e.clientX - rect.left - rect.width / 2;
-                const y = e.clientY - rect.top - rect.height / 2;
+    item.style.transform = `translate(${x * 0.3}px, ${y * 0.3}px)`;
+  });
 
-                item.style.transform = `translate(${x * 0.3}px, ${y * 0.3}px)`;
-            });
+  item.addEventListener("mouseleave", function () {
+    item.style.transform = "translate(0, 0)";
+  });
+});
 
-            item.addEventListener("mouseleave", function () {
-                item.style.transform = "translate(0, 0)";
-            });
+// AJAX success script
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    let form = this;
 
-        })
+    fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+      headers: { Accept: "application/json" },
+    })
+      .then((response) => {
+        alert("Message sent successfully!");
+        form.reset();
+      })
+      .catch((error) => alert("Something went wrong."));
+  });
